6d2e3c2f7d96ac463ff7e6ede8445612cb7bf027
Henry Baquiax <henrybaquiax@umes.edu.gt>
2024-05-19 18:33:44 -0600

    Clases

---
+++ Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Lab14_Ejer2_Repaso
+{
+    public class Album
+    {
+        public string Titulo { get; set; }
+        public string Artista { get; set; }
+        public List<Cancion> Canciones { get; set; } 
+        public DateTime FechaPublicacion { get; set; }
+
+        public Album()
+        {
+            Titulo = string.Empty;
+            Artista = string.Empty;
+            Canciones = new List<Cancion>();
+            FechaPublicacion = DateTime.Now;
+        }
+    }
+}
---
+++ Cancion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lab14_Ejer2_Repaso
+{
+    public class Cancion
+    {
+        public string Nombre { get; set; }
+        public string Artista { get; set; }
+        public TimeSpan Duracion { get; set; }
+
+        public Cancion()
+        {
+            Nombre = string.Empty;
+            Artista = string.Empty;
+            Duracion = TimeSpan.Zero;
+        }
+    }
+}
